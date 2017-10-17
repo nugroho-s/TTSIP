268aa9846098d34d91321bcf701479ad11a61cad
nugroho-s <massatriya@gmail.com>
2017-10-17 22:25:48 +0700

    remove deprecated callback

--- app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/FacePictureCallback.java
+++ app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/FacePictureCallback.java
@@ -9,13 +9,8 @@ import com.google.android.gms.vision.CameraSource;
  * Created by nugsky on 17/10/17.
  */
 
-public class FacePictureCallback implements Camera.PictureCallback, CameraSource.PictureCallback {
+public class FacePictureCallback implements CameraSource.PictureCallback {
     static String logId = "FacePictureCallback";
-
-    @Override
-    public void onPictureTaken(byte[] bytes, Camera camera) {
-        Log.d(logId,"called callback camera");
-    }
 
     @Override
     public void onPictureTaken(byte[] bytes) {
